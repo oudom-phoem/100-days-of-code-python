230587a159e9a74c6547147b0c4febcf8e78cf73
Oudom Phoem <oudomphoem@gmail.com>
2024-06-25 00:50:16 +0700

    Complete day 2

--- day02/main.py
+++ day02/main.py
@@ -1,1 +1,9 @@
-print(3 * 3 + 3 / 3 - 3)
+print("Welcome to the tip calculator!")
+bill = int(input("What was the total bill? $"))
+tip_percent = int(input("How much would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+tip_amount = bill * tip_percent / 100
+total_bill = bill + tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+print(f"Each person should pay: ${bill_per_person:.2f}")
