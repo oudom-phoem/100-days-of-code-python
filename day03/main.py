dbcb79799e46eeff2a1d17fb830f793ee592c8e9
Oudom Phoem <oudomphoem@gmail.com>
2024-06-25 15:35:08 +0700

    Complete day 3

--- day03/main.py
+++ day03/main.py
@@ -22,8 +22,20 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
-
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
-
-#Write your code below this line 👇
-
+step1 = input("You're at a cross road. Where do you want to go? Type \"left\" or \"right\"\n")
+if(step1 == "left"):
+    step2 = input("You've come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim across.\n")
+    if(step2 == "wait"):
+        step3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?\n")
+        if(step3 == "red"):
+            print("It's a room full of fire. Game Over.")
+        elif(step3 == "yellow"):
+            print("You found the treasure! You Win!")
+        elif(step3 == "blue"):
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("Game Over.")
+    else:
+        print("You get attacked by an angry trout. Game Over.")
+else:
+    print("You fell into a hole. Game Over.")
